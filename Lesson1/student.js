551879588c564ed131441619de9c3ec02afc0dc5
lina-22 <haquefahinur@gmail.com>
2023-10-16 00:48:03 +0200

    Module | Local module - exports, require

---
+++ Lesson1/student.js
@@ -0,0 +1,20 @@
+const getName = () => {
+  return "Lina haque";
+};
+
+exports.getAge = () => {
+  return "25";
+};
+const cgpa = 3.93;
+
+// exports.getName = getName;
+
+// exports.getAge = getAge;
+
+// exports.result = cgpa;
+
+module.exports = {
+  getName,
+  getAge,
+  cgpa,
+};
